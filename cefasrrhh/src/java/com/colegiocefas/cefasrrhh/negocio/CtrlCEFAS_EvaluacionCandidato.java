074f1bab2ce66617f8c1100b83afdf25e5cefb2e
sergiogalvez19fs@gmail.com <sergiogalvez19fs@gmail.com@eaaebd86-1559-794a-01cf-8a87f12dd982>
2014-11-11 21:05:02 +0000

    sergio

--- cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionCandidato.java
+++ cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionCandidato.java
@@ -6,10 +6,29 @@
 
 package com.colegiocefas.cefasrrhh.negocio;
 
-/**
- *
- * @author Portillo
- */
+import com.colegiocefas.cefasrrhh.datos.CEFAS_EvaluacionCandidatoDAO;
+import com.colegiocefas.cefasrrhh.dominio.CEFAS_EvaluacionCandidato;
+import java.util.Date;
+
 public class CtrlCEFAS_EvaluacionCandidato {
-    
+
+    private CEFAS_EvaluacionCandidatoDAO daoEvaluacionCandidato = new CEFAS_EvaluacionCandidatoDAO();
+
+    public void guardarEvaluacion(String DUI, String ecvCodigo, String evcObservacionesReferencias, Date evcFechaEntrevista, String evcComentarioEntrevista, Date evcFechaExamenAptitud, String evcNotaExamenAptitud, String evcComentarioExamenAptitud, Date evcFechaExamenPsico, String evcNotaExamenPsico, String evcComentarioExamenPsico) {
+
+        CEFAS_EvaluacionCandidato evaluacioncan = new CEFAS_EvaluacionCandidato();
+
+        evaluacioncan.setCdtDUI(DUI);
+        evaluacioncan.setEvcCodigo(ecvCodigo);
+        evaluacioncan.setEvcObservacionesReferencias(evcObservacionesReferencias);
+        evaluacioncan.setEvcFechaEntrevista(evcFechaEntrevista);
+        evaluacioncan.setEvcComentarioEntrevista(evcComentarioEntrevista);
+        evaluacioncan.setEvcFechaExamenAptitud(evcFechaExamenAptitud);
+        evaluacioncan.setEvcNotaExamenAptitud(evcNotaExamenAptitud);
+        evaluacioncan.setEvcComentarioExamenAptitud(evcComentarioExamenAptitud);
+        evaluacioncan.setEvcFechaExamenPsico(evcFechaExamenPsico);
+        evaluacioncan.setEvcFechaExamenPsico(evcFechaExamenPsico);
+        evaluacioncan.setEvcCometarioExamenPsico(evcComentarioExamenPsico);
+        daoEvaluacionCandidato.almacenarEvaluacion(evaluacioncan);
+    }
 }
