16cb2443085c1534ab223368adec6d03caec8d1f
migueljuarez2007@hotmail.com <migueljuarez2007@hotmail.com@eaaebd86-1559-794a-01cf-8a87f12dd982>
2014-11-12 19:19:17 +0000

    evaluaciones

--- trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionDeClases.java
+++ trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionDeClases.java
@@ -6,10 +6,44 @@
 
 package com.colegiocefas.cefasrrhh.negocio;
 
+import com.colegiocefas.cefasrrhh.datos.CEFAS_EvaluacionDeClasesDAO;//CEFAS_EvaluacionDeClases
+import com.colegiocefas.cefasrrhh.dominio.CEFAS_EvaluacionDeClases;
+import java.util.Date;
+import java.util.List;
+
+
 /**
  *
  * @author Portillo
  */
 public class CtrlCEFAS_EvaluacionDeClases {
+
+	CEFAS_EvaluacionDeClasesDAO evalcDao = new CEFAS_EvaluacionDeClasesDAO ();
     
+     public boolean guardar(Date fecha, String ruta)
+    {
+        CEFAS_EvaluacionDeClases evalc = new CEFAS_EvaluacionDeClases();
+        evalc.setEclFecha(fecha);
+        evalc.setEclRutaArchivo(ruta);
+        return evalcDao.guardar(evalc);
+    }
+    
+    public List<CEFAS_EvaluacionDeClases> obtenerEvaluacionPorFuncionesById()
+    {
+        return evalcDao.obtenerEvaluacionDeClasesById();
+    }
+    
+   /*
+    public void actualizar(Date fecha, String ruta)
+    {
+        CEFAS_EvaluacionDeClases evalc = new CEFAS_EvaluacionDeClases(id, fecha, ruta);
+        evalDao.actualizar(evalc);
+    }*/
+    
+    public void eliminarActividadById(String id)
+    {
+        evalcDao.eliminarById(id);
+    }
+   
+
 }
--- trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionPorCompetencias.java
+++ trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionPorCompetencias.java
@@ -6,10 +6,45 @@
 
 package com.colegiocefas.cefasrrhh.negocio;
 
+import com.colegiocefas.cefasrrhh.datos.CEFAS_EvaluacionPorCompetenciasDAO;
+import com.colegiocefas.cefasrrhh.dominio.CEFAS_EvaluacionPorCompetencias;
+import java.util.Date;
+import java.util.List;
+
+
 /**
  *
  * @author Portillo
  */
 public class CtrlCEFAS_EvaluacionPorCompetencias {
     
+        CEFAS_EvaluacionPorCompetenciasDAO evalDao = new CEFAS_EvaluacionPorCompetenciasDAO ();
+
+    
+     public boolean guardar(Date fecha, String ruta)
+    {
+        CEFAS_EvaluacionPorCompetencias eval = new CEFAS_EvaluacionPorCompetencias();
+        eval.setEcoFecha(fecha);
+        eval.setEcoRutaArchivo(ruta);
+        return evalDao.guardar(eval);
+    }
+    
+    public List<CEFAS_EvaluacionPorCompetencias> obtenerEvaluacionPorcompetenciasById()
+    {
+        return evalDao.obtenerEvaluacionPorcompetenciasById();
+    }
+    
+  
+   /* public void actualizar(Date fecha, String ruta)
+    {
+        CEFAS_EvaluacionPorCompetencias eval = new CEFAS_EvaluacionPorCompetencias(id, fecha, ruta);
+        evalDao.actualizar(eval);
+    }*/
+    
+    public void eliminarActividadById(String id)
+    {
+        evalDao.eliminarById(id);
+    }
+   
+
 }
