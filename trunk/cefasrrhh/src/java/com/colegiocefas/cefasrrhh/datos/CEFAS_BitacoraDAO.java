b5d4ca9ec05a9d9414b6637f83da8d09d25700f6
migueljuarez2007@hotmail.com <migueljuarez2007@hotmail.com@eaaebd86-1559-794a-01cf-8a87f12dd982>
2014-11-24 17:22:06 +0000

    Bitacora de momento solo para anticipo, en la tarde esta para los demas

--- trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/datos/CEFAS_BitacoraDAO.java
+++ trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/datos/CEFAS_BitacoraDAO.java
@@ -6,10 +6,53 @@
 
 package com.colegiocefas.cefasrrhh.datos;
 
+
+import com.colegiocefas.cefasrrhh.dominio.CEFAS_Bitacora;
+import com.colegiocefas.cefasrrhh.utilidades.ConexionDB;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Portillo
  */
 public class CEFAS_BitacoraDAO {
+    private final String SQL_INSERT = "INSERT INTO CEFAS_BITACORA (USRCODIGO, BTCFECHAHORA, BTCDESCRIPCION) VALUES (?, ?, ?)";
+    private Connection conexiondb;
+    private Statement st;
+    private PreparedStatement ps;
+    private ResultSet rs;
+      
+    
+    public void guardarAccion(CEFAS_Bitacora bitacora) {
+     //almacena un anticipo leido desde el navegador
+    
+        try {
+            conexiondb = ConexionDB.getConexion();
+            ps=conexiondb.prepareStatement(SQL_INSERT);
+            ps.setInt(1, bitacora.getUserCodigo());
+            ps.setDate(2, new Date(bitacora.getBtcFechaHora().getTime()));
+            ps.setString(3, bitacora.getBtcDescripcion());
+            int n=ps.executeUpdate();
+
+            if(n>0){
+               Logger.getLogger("Se guardo correctamente");
+            }
+ConexionDB.cerrarConexion();
+        } catch (SQLException ex) {
+            Logger.getLogger(CEFAS_BitacoraDAO.class.getName()).log(Level.SEVERE, null, ex);
+             
+        }
+     
+     
+     }
     
 }
--- trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_Bitacora.java
+++ trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_Bitacora.java
@@ -6,10 +6,26 @@
 
 package com.colegiocefas.cefasrrhh.negocio;
 
+import com.colegiocefas.cefasrrhh.datos.CEFAS_BitacoraDAO;
+import com.colegiocefas.cefasrrhh.dominio.CEFAS_Actividad;
+import com.colegiocefas.cefasrrhh.dominio.CEFAS_Bitacora;
+import java.util.Date;
+
 /**
  *
  * @author Portillo
  */
 public class CtrlCEFAS_Bitacora {
+    
+    CEFAS_BitacoraDAO daoBitacora= new CEFAS_BitacoraDAO();
+    
+    public void guardarBitacora(int codigoUsuario,  String descripcion){
+        Date fecha=new Date();
+        CEFAS_Bitacora bitacora= new CEFAS_Bitacora();
+        bitacora.setUserCodigo(codigoUsuario);
+        bitacora.setBtcFechaHora(fecha);
+        bitacora.setBtcDescripcion(descripcion);
+        daoBitacora.guardarAccion(bitacora);
+    }
     
 }
