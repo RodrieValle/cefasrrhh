fbd67749c420bf6aef7baa0d6bea1a4c8b6cb1a1
sergiogalvez19fs@gmail.com <sergiogalvez19fs@gmail.com@eaaebd86-1559-794a-01cf-8a87f12dd982>
2014-11-24 05:18:05 +0000

    sergio

---
+++ trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/dominio/CEFAS_Especialidad.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.colegiocefas.cefasrrhh.dominio;
+
+/**
+ *
+ * @author Sergio
+ */
+public class CEFAS_Especialidad {
+    private int espCodigo;
+    private String espNombre;
+    
+    public CEFAS_Especialidad(){
+        
+    }
+    public CEFAS_Especialidad(int espCodigo,String espNombre){
+        this.espCodigo=espCodigo;
+        this.espNombre=espNombre;
+    }
+    public int getEspCodigo(){
+        return espCodigo;
+    }
+    public void setEspCodigo(int espCodigo){
+        this.espCodigo=espCodigo;
+    }
+    
+}
---
+++ trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_Especialidad.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.colegiocefas.cefasrrhh.negocio;
+
+/**
+ *
+ * @author Sergio
+ */
+public class CtrlCEFAS_Especialidad {
+    
+}
