2726288c53f728379e038d4ff406f61f357b7a98
rodrigoahv@hotmail.com <rodrigoahv@hotmail.com@eaaebd86-1559-794a-01cf-8a87f12dd982>
2014-11-26 07:10:06 +0000

    Avance el informes

--- trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionPorCompetencias.java
+++ trunk/cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_EvaluacionPorCompetencias.java
@@ -21,9 +21,10 @@ public class CtrlCEFAS_EvaluacionPorCompetencias {
         CEFAS_EvaluacionPorCompetenciasDAO evalDao = new CEFAS_EvaluacionPorCompetenciasDAO ();
 
     
-     public boolean guardar(Date fecha, String ruta)
+     public boolean guardar(String codigoEmpleado, Date fecha, String ruta)
     {
         CEFAS_EvaluacionPorCompetencias eval = new CEFAS_EvaluacionPorCompetencias();
+        eval.setEmpCodigo(codigoEmpleado);
         eval.setEcoFecha(fecha);
         eval.setEcoRutaArchivo(ruta);
         return evalDao.guardar(eval);
