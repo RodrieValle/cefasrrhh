0980cf0e1d467f716c70c13d4234afceca7036ae
sergiogalvez19fs@gmail.com <sergiogalvez19fs@gmail.com@eaaebd86-1559-794a-01cf-8a87f12dd982>
2014-11-26 07:40:06 +0000

    sergio

--- cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_Sancion.java
+++ cefasrrhh/src/java/com/colegiocefas/cefasrrhh/negocio/CtrlCEFAS_Sancion.java
@@ -18,14 +18,14 @@ import java.util.List;
 public class CtrlCEFAS_Sancion {
     private CEFAS_SancionDAO daoSancion = new CEFAS_SancionDAO();
     
-    public void guardarSancion(int empCodigo,int scnCodigo,Date scnFecha,String scnMotivo,String scnSancion){
+    public void guardarSancion(int empCodigo,int scnCodigo,Date scnFecha,String scnSancion,String scnMotivo){
         CEFAS_Sancion scn= new CEFAS_Sancion();
          
                 scn.setEmpCodigo(empCodigo);
                 scn.setScnCodigo(scnCodigo);
                 scn.setScnFecha(scnFecha);
-                scn.setScnMotivo(scnMotivo);
                 scn.setScnSancion(scnSancion);
+                scn.setScnMotivo(scnMotivo);
                
         daoSancion.almacenarSancion(scn);
     }
