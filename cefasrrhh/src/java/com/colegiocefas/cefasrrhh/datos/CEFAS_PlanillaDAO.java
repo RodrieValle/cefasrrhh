163a89a279d4bb1ddacd543cc4d2ea093b2e3893
rodrigoahv@hotmail.com <rodrigoahv@hotmail.com@eaaebd86-1559-794a-01cf-8a87f12dd982>
2014-12-01 09:04:27 +0000

    Cambios en reportes

--- cefasrrhh/src/java/com/colegiocefas/cefasrrhh/datos/CEFAS_PlanillaDAO.java
+++ cefasrrhh/src/java/com/colegiocefas/cefasrrhh/datos/CEFAS_PlanillaDAO.java
@@ -24,6 +24,7 @@ import java.util.logging.Logger;
 public class CEFAS_PlanillaDAO {
      private final String SQL_INSERT = "INSERT INTO CEFAS_PLANILLA (PLNTIPO, PLNFECHA) VALUES (?, ?)";
     private final String SQL_SELECTALL = "SELECT * FROM CEFAS_PLANILLA";
+    private final String SQL_SELECT_ID = "SELECT * FROM CEFAS_PLANILLA WHERE PLNCODIGO = ?";
      private final String SQL_SELECT_DATE = "SELECT * from CEFAS_PLANILLA WHERE PLNCODIGO=(SELECT MAX(PLNCODIGO) from CEFAS_PLANILLA)";
     private final String SQL_SELECTBYID = "";
     private final String SQL_UPDATE = "";
@@ -103,6 +104,28 @@ ConexionDB.cerrarConexion();
         }
         return listPlanillas;
     }
+     
+     public CEFAS_Planilla getPlanillaPorID(int plncodigo){
+         CEFAS_Planilla planilla = null;
+        try {
+            conexiondb = ConexionDB.getConexion();
+            ps=conexiondb.prepareStatement(SQL_SELECT_ID);
+            ps.setInt(1, plncodigo);
+            rs = ps.executeQuery();
+            while(rs.next())
+            {
+                planilla= new CEFAS_Planilla();
+                planilla.setPlnCodigo(rs.getInt("plnCodigo"));
+                planilla.setPlnTipo(rs.getInt("plnTipo"));
+                planilla.setPlnFecha(rs.getDate("plnFecha"));
+            }
+             ConexionDB.cerrarConexion();
+        } catch (SQLException ex) {
+            Logger.getLogger(CEFAS_PlanillaDAO.class.getName()).log(Level.SEVERE, null, ex);
+             
+        }
+     return planilla;
+     }
     
      
 }
--- cefasrrhh/web/planilla.jsp
+++ cefasrrhh/web/planilla.jsp
@@ -1,3 +1,5 @@
+<%@page import="com.colegiocefas.cefasrrhh.dominio.CEFAS_Planilla"%>
+<%@page import="com.colegiocefas.cefasrrhh.negocio.CtrlCEFAS_Planilla"%>
 <%@page import="com.colegiocefas.cefasrrhh.utilidades.ConexionDB"%>
 <%@page import="java.text.DateFormat"%>
 <%@page import="java.sql.ResultSet"%>
@@ -14,10 +16,29 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 
 <%
+    String meses[] = {"ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO",
+        "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"}; 
     int idPlanilla = Integer.parseInt(request.getParameter("idPlanilla"));
     Map parametros = new HashMap();
     parametros.put("idPlanilla", idPlanilla);
     Connection conn = ConexionDB.getConexion();
+    
+    CtrlCEFAS_Planilla ctrlPlanilla = new CtrlCEFAS_Planilla();
+    CEFAS_Planilla planilla = ctrlPlanilla.getPlanillaPorID(idPlanilla);
+    String datos[] = new SimpleDateFormat("dd/MM/yyyy").format( planilla.getPlnFecha()).split("/");
+    int anio = Integer.parseInt(datos[2]);
+    int mes = Integer.parseInt(datos[1]);
+    int dias = Integer.parseInt(datos[0]);
+    if(dias<=2)
+        mes--;
+    if(mes==0)
+    {
+        mes = 12;
+        anio--;
+    }
+        
+    String periodo = meses[mes-1] + " " + anio;
+    parametros.put("periodo", periodo);
     
     //Obteniendo la fecha actual
     Calendar calendario = Calendar.getInstance();
